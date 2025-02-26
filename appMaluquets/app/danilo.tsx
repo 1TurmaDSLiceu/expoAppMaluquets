eb774996565e752acffaf41a50fb03ca6c7ee5eb
unknown <admin@LICLABTEC-13.adserver.unisantos.br>
2025-02-25 21:34:55 -0300

    commit danilo

--- appMaluquets/app/danilo.tsx
+++ appMaluquets/app/danilo.tsx
@@ -1,15 +1,15 @@
 import { Text, StyleSheet, View, Button } from 'react-native'
 import { useBatteryLevel, usePowerState } from 'expo-battery';
 import PagerView from 'react-native-pager-view';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 import { FontAwesome } from '@expo/vector-icons';
-
 
 export default function Danilo() {
     const { lowPowerMode, batteryLevel, batteryState } = usePowerState();
+    let bateria = 1
 
-    
     let buttonColor = 'green';
-    if(batteryLevel > 0.6) {
+    if (batteryLevel > 0.6) {
         buttonColor = 'green';
     } else {
         buttonColor = 'red';
@@ -24,38 +24,58 @@ export default function Danilo() {
                 <PagerView style={styles.container} initialPage={0}>
                     <View style={styles.page} key="1">
                         <Text style={styles.subtitulo}>Nível atual de bateria</Text>
-                        <View style={{...styles.batteryLevel, backgroundColor:`${buttonColor}`}}>
+                        <View style={{ ...styles.batteryLevel, backgroundColor: `${buttonColor}` }}>
                             <Text style={styles.subtitulo}>{batteryLevel * 100}%</Text>
                         </View>
-                        <Text>Araxta pro lado ➡️</Text>
                     </View>
                     <View style={styles.page} key="2">
-                        <Text style={styles.subtitulo}>Modo pouca energia ligado?</Text>
+                        <Text >Modo pouca energia ligado?</Text>
                         {
                             lowPowerMode
-                            ?
-                           <FontAwesome name="toggle-on"  size={50} color="black" />
-                            :
-                             <FontAwesome name="toggle-on"  size={50} color="black" />
+                                ?
+                                <FontAwesome name="toggle-on" size={50} color="white" />
+                                :
+                                <FontAwesome name="toggle-on" size={50} color="white" />
                         }
                     </View>
                     <View style={styles.page} key="3">
-                        <Text style={styles.subtitulo}>Esse celular ta carregando?</Text>
-                       
-                        <FontAwesome name="battery-half" size={50} color="orange" />
+                        <Text style={styles.subtitulo}>Como esta o estado da bateria?</Text>
+                        {batteryState === 0 ?
+                            <View style={styles.batteryState}>
+                                <MaterialIcons name="battery-unknown" size={50} color="white" />
+                                <Text style={styles.subtitulo}>Desconhecido</Text>
+                            </View>
+                            :
+                            null
+                        }
+                        {batteryState === 1 ?
+                            <View style={styles.batteryState}>
+                                <MaterialIcons name="battery-4-bar" size={50} color="white" />
+                                <Text style={styles.subtitulo}>Desplugado</Text>
+                            </View>
+                            :
+                            null
+                        }
+                        {batteryState === 2 ?
+                            <View style={styles.batteryState}>
+                                <MaterialIcons name="battery-charging-full" size={50} color="white" />
+                                <Text style={styles.subtitulo}>Carregando</Text>
+                            </View>
+                            :
+                            null
+                        }
+                        {batteryState === 3 ?
+                            <View style={styles.batteryState}>
+                                <MaterialIcons name="battery-full" size={50} color="white" />
+                                <Text style={styles.subtitulo}>Completa</Text>
+                            </View>
+                            :
+                            null
+                        }
                     </View>
                 </PagerView>
             </View>
-
-
-
-
         </View>
-
-
-
-
-
     )
 }
 
@@ -64,7 +84,7 @@ const styles = StyleSheet.create({
     container: {
         width: '100%',
         height: '100%',
-        backgroundColor: 'gray',
+        backgroundColor: '#01001f',
         display: 'flex',
         justifyContent: 'flex-start',
         alignItems: 'center',
@@ -77,18 +97,17 @@ const styles = StyleSheet.create({
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'center',
-        backgroundColor: 'red'
     },
 
     batteryLevel: {
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
-        borderWidth:2,
+        borderWidth: 2,
         width: '20%',
-        height: 50,
+        height: 100,
         borderColor: 'white',
-        
+
     },
 
     pageView: {
@@ -97,20 +116,30 @@ const styles = StyleSheet.create({
     },
 
     titulo: {
-        fontSize: 30
+        fontSize: 30,
+        color:'white'
     },
 
     subtitulo: {
         fontSize: 20,
-        
+        color:'white'
+
+    },
+    
+    batteryState: {
+        display:'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: 20
     },
 
     page: {
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: 'purple',
+        backgroundColor: '#010133',
         gap: 10,
-        height: "20%",
+        height: "40%",
         padding: 10
     },
 })
